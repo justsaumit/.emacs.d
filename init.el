c521d8568356edd890ab02f384a82341c0f4dc86
Saumit Dinesan <justsaumit@protonmail.com>
2023-03-06 12:27:00 +0530

    init.el: melpa+use-package

--- init.el
+++ init.el
@@ -8,3 +8,30 @@
 ;; Set JetBrains mono as default font
 (add-to-list 'default-frame-alist
 	     '(font . "JetBrainsMono Nerd Font Mono-14"))
+
+;; Melpa setup
+(require 'package)
+(setq package-enable-at-startup nil)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; Easy Package Management
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;Auto written by emacs
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(dashboard use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
