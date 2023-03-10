c7f5e12ac7eda118bd8695aa1892bc2b3040642c
Saumit Dinesan <justsaumit@protonmail.com>
2023-03-10 18:02:34 +0530

    init.el: linenumber-config + rainbow-delimiters + which-key

--- init.el
+++ init.el
@@ -43,8 +43,14 @@
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
 
 ;; relativenumberline
-(global-display-line-numbers-mode)
+(global-display-line-numbers-mode t) ;; only for textfiles not shell buffers
 (setq display-line-numbers-type 'relative)
+;;Disable line numbers for certain modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; Quickly access init.el
 (global-set-key (kbd "C-c d")
@@ -98,6 +104,10 @@
   (setq dashboard-startup-banner "~/.emacs.d/dracon.png")
   (setq dashboard-banner-logo-title "while(!(succeed = try() ) );"))
 
+;; Rainbow delimiters
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
 ;;Smex - M-x enhancer
 (use-package smex
   :ensure t
@@ -142,20 +152,32 @@
   :custom (doom-modeline height 15))
 (use-package all-the-icons
   :ensure t)
-
+;; doom-themes
 (use-package doom-themes
-      :if window-system
-      :ensure t
-      :config
-      (load-theme 'doom-tokyo-night t)
-      (doom-themes-org-config))
+  :if window-system
+  :ensure t
+  :config
+  (load-theme 'doom-tokyo-night t)
+  (doom-themes-org-config))
+
+;; which-key
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
 ;;Auto written by emacs
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(lsp-java lsp-mode dashboard use-package)))
+ '(package-selected-packages
+   '(which-key rainbow-delimiters lsp-java lsp-mode dashboard use-package))
+'(custom-safe-themes
+   '("bf948e3f55a8cd1f420373410911d0a50be5a04a8886cabe8d8e471ad8fdba8e" "5586a5db9dadef93b6b6e72720205a4fa92fd60e4ccfd3a5fa389782eab2371b" "ff24d14f5f7d355f47d53fd016565ed128bf3af30eb7ce8cae307ee4fe7f3fd0" "b99e334a4019a2caa71e1d6445fc346c6f074a05fcbb989800ecbe54474ae1b0" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "8d8207a39e18e2cc95ebddf62f841442d36fcba01a2a9451773d4ed30b632443" "6945dadc749ac5cbd47012cad836f92aea9ebec9f504d32fe89a956260773ca4" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "be84a2e5c70f991051d4aaf0f049fa11c172e5d784727e0b525565bb1533ec78" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" default))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
